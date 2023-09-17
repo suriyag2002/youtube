ac478a7094c247c3c09a990aae58e76e2cdb9f4f
git-test <suriya.gunasekaran2002@gmail.com>
2023-09-17 21:41:51 +0530

    commit

---
+++ src/utils/fetchFromAPI.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+export const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
+
+const options = {
+  params: {
+    maxResults: 50,
+  },
+  headers: {
+    'X-RapidAPI-Key': 'd21e8a9c0amsha1889099535d677p1f8a59jsna6471b3dc4a2',
+    'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
+  },
+};
+
+export const fetchFromAPI = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+
+  return data;
+};
+
+  // part: 'snippet',
+  // videoId: 'M7FIvfx5J10'
